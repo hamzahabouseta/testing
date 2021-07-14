14f350da57cdaf1b0ad0fe48f874f59f3dc77d94
Hamzah Abouseta <55253746+hamzahabouseta@users.noreply.github.com>
2021-07-14 17:09:02 -0400

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,50 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+    
+    <div class="topnav">
+  <a href="/">Home</a>
+  <a href="/page-2/">Go to Page 2</a>
+  <a href="/using-typescript/">Go to using Typescript</a>
+</div>
+  </header>
+)
+
+
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
